1921beba592dea72fada4bd546dd5ae56351a10a
Extruded <extrude57@gmail.com>
2017-09-29 04:02:23 -0700

    Start of the Game
    The beginning of the game all over again  for the 2nd time WITHOUT source tree this time. Bet things will go as planed this time around.

---
+++ Battle_Of_The_Tanks/Source/Battle_Of_The_Tanks/Battle_Of_The_TanksGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Battle_Of_The_Tanks.h"
+#include "Battle_Of_The_TanksGameModeBase.h"
+
+
+
+
---
+++ Battle_Of_The_Tanks/Source/Battle_Of_The_Tanks/Battle_Of_The_TanksGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "Battle_Of_The_TanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLE_OF_THE_TANKS_API ABattle_Of_The_TanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
